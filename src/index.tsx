1b0988495090b09c9241be25fc7ef52b75ffb3ac
DGUHJH <ghkdwogud852@gmail.com>
2023-06-01 17:08:40 +0900

    fix : BrowserRouter 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { MainPage } from 'pages/Main'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element)
 root.render(
   <React.StrictMode>
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 )
